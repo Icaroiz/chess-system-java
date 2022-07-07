4d649134314449696e169ec21ef9290e016b38d4
Icaro Gabriel <icaroizext@gmail.com>
2022-07-07 11:21:50 -0300

    CheckMate

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -19,7 +19,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -50,6 +50,8 @@ public class Program {
 		
 			}	
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
 
--- src/aplication/UI.java
+++ src/aplication/UI.java
@@ -56,12 +56,17 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn :" + chessMatch.getTurn());
-		System.out.println("Waiting player:  " + chessMatch.getCurrentPlayer());
-		if(chessMatch.getCheck()) {
-			System.out.println("CHECK !");
+		if(!chessMatch.getCheckMate()) {
+			System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+			if(chessMatch.getCheck()) {
+				System.out.println("CHECK!");
 		}
 	}
-	
+		else {
+			System.out.println("CHECKMATE !");
+			System.out.println("WINNER ! " + chessMatch.getCurrentPlayer());
+	}
+}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i<pieces.length; i++) {
